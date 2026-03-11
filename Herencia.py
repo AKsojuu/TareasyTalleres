cfa395366f3b0b0dee856e457254e34611998fd6
Mathikaze <mathiasmath000@gmail.com>
2026-03-11 09:12:42 -0500

    Implement basic store GUI with Tkinter
    Refuerzo de herencia en python con GUI en Tkinter

---
+++ Herencia.py
@@ -0,0 +1,73 @@
+import tkinter as tk
+    
+class tiendabase(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        
+        self.title("Ventana Base")
+        self.geometry("400x400")
+        self.config(bg="RED")
+
+
+class Tienda(tiendabase):
+    def __init__(self):
+        super().__init__()
+        
+        self.title = tk.Label(
+            self,
+            text="Tienda la veci",
+            font=("Arial",16,"bold"),
+            
+        )
+        self.title.pack(pady=10)
+        
+        
+        self.producto1= tk.Label(
+            self,
+            text="Bebidas",
+            font=("Arial",16,"bold"),
+            bg="BLUE    ",
+            fg="GREEN"
+        )
+        self.producto1.pack()
+
+
+        #   Crear el objeto y simplemente adaptarlo        
+        self.lista_bebidas=tk.Listbox(
+            self,
+            width=50,
+            height=8,
+            font=("Arial",10)
+        )
+        self.lista_bebidas.pack(pady=5)
+        
+        
+        #   Ingresar productos
+        self.lista_bebidas.insert(tk.END,"Coca Cola     -   1$")
+        self.lista_bebidas.insert(tk.END,"Te        -       2$")
+        
+
+        
+        self.producto2= tk.Label(
+            self,
+            text="Comida",
+            font=("Arial",12,"bold"),
+            bg="BLUE",
+            fg="GREEN",
+            width=50,
+            height=8
+        )
+        self.producto2.pack(pady=5)
+        
+        self.lista_comida=tk.Listbox(
+            self,
+            width=50,
+            height=8,
+            font=("Arial",10)
+        )
+        self.lista_comida.pack(pady=5)
+        
+        self.lista_comida.insert(tk.END,"Encebollado    -   2#")# END: Al final del ultimo registro ingresa esto
+        self.lista_comida.insert(tk.END,"Sushi      -       1$")
+        
+app.mainloop
